93faf3c06efb5c2b7675d912b2b5c247621cf95d
Blagovest Ivanov <ivanovb2@uni.coventry.ac.uk>
2021-04-05 23:33:54 +0100

    Fix post request for the basket

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,25 +1,26 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { useEffect, useState } from 'react'
-import {Text, View, StyleSheet, Button, Image, TouchableOpacity} from 'react-native'
+import { Text, View, StyleSheet, Button, Image, TouchableOpacity } from 'react-native'
 import { connect } from 'react-redux'
-import {Entypo} from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 import { AntDesign } from '@expo/vector-icons';
 import { Feather } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 
 const BASE_URL = 'http://138.68.166.198';
 
-function CartItems(props){
+function CartItems(props) {
     const [errorMessage, setErrorMessage] = useState(null);
     const [response, setResponse] = useState(null);
 
-    async function load(){
+    async function load() {
         try {
             const response_server = await createOrder(props.cartItems);
             const result = await response_server.json();
-            if (response.ok) {
+            if (response_server.ok) {
                 setResponse(result);
-                cart = []
+                console.log('helllo')
+                props.cartItems = []
             } else {
                 setErrorMessage(result);
             }
@@ -27,55 +28,56 @@ function CartItems(props){
             setErrorMessage(err.message);
         }
     }
-    async function createOrder(cart){
+    async function createOrder(cart) {
         const url = `${BASE_URL}/orders`
         const username = await AsyncStorage.getItem('username')
         const token = await AsyncStorage.getItem('userToken')
-        let cart_server = cart.map((item) => {                       
-            return { id: item.id, description: item.description, qty: item.qty} 
+        let cart_server = cart.map((item) => {
+            return { id: item.id, description: item.description, qty: item.qty }
         })
-        console.log(cart_server)
-        const body = JSON.stringify({status: 'active', basket: cart_server})
+        const basket = cart_server
+        const status = 'active'
         return await fetch(url,
             {
                 method: 'POST',
                 mode: 'cors',
                 headers: {
-                    'Authorization': token
+                    'Authorization': token,
+                    'Content-Type': 'application/json'
                 },
-                body: body
+                body: JSON.stringify({ status, basket })
             });
     }
 
-    function renderProducts(products){
+    function renderProducts(products) {
         return products.map((item, index) => {
             return (
-                <View style={{display:'flex', flexDirection:'row', backgroundColor:'007aff', marginTop:'5%', marginLeft:'1%', marginRight:'1%'}} key={index}>
-                    <Image source={{uri: `${item.product_image}`}} style={{display:'flex', width: 100, height: 100, resizeMode: 'contain'}} />
+                <View style={{ display: 'flex', flexDirection: 'row', backgroundColor: '007aff', marginTop: '5%', marginLeft: '1%', marginRight: '1%' }} key={index}>
+                    <Image source={{ uri: `${item.product_image}` }} style={{ display: 'flex', width: 100, height: 100, resizeMode: 'contain' }} />
 
-                    <View style={{display:'flex', alignItems:'flex-start', flexDirection:'column', flexGrow:1, justifyContent:'center', }}>
+                    <View style={{ display: 'flex', alignItems: 'flex-start', flexDirection: 'column', flexGrow: 1, justifyContent: 'center', }}>
                         <Text style={styles.itemDescription}>{item.description}</Text>
                         <Text style={styles.itemPrice}>£{item.price}</Text>
                         <Text style={styles.itemQty}>Qty: {item.qty}</Text>
                     </View>
-                    <View title={"Button"} style={{display:'flex', alignItems:'flex-end'}}>
+                    <View title={"Button"} style={{ display: 'flex', alignItems: 'flex-end' }}>
                         <TouchableOpacity
-                            style={{marginTop:'50%'}}
+                            style={{ marginTop: '50%' }}
                             onPress={() => props.adjustQty(item.id, item.qty + 1)}>
                             <AntDesign name="plus" size={24} color="black" />
 
                         </TouchableOpacity>
 
                         <TouchableOpacity
-                            style={{marginTop:'50%'}}
+                            style={{ marginTop: '50%' }}
                             onPress={() => (item.qty - 1) !== 0 ? props.adjustQty(item.id, item.qty - 1) : props.removeItem(item)} >
                             <AntDesign name="minus" size={24} color="black" />
 
                         </TouchableOpacity>
 
-                        <TouchableOpacity style={{marginTop:'50%', fontFamily:'Helvetica',}}
+                        <TouchableOpacity style={{ marginTop: '50%', fontFamily: 'Helvetica', }}
                             onPress={() => props.removeItem(item)} title={'Remove from cart'}>
-                        <Feather name="trash-2" size={24} color="black" />
+                            <Feather name="trash-2" size={24} color="black" />
                         </TouchableOpacity>
                     </View>
 
@@ -93,7 +95,7 @@ function CartItems(props){
         })
     }
 
-    function renderButton(products){
+    function renderButton(products) {
         let total = 0
         products.map((item) => {
             total = total + item.price * item.qty
@@ -130,7 +132,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         removeItem: (product) => dispatch({ type: 'REMOVE_FROM_CART', payload: product }),
-        adjustQty: (product_id, value) => dispatch({ type: 'ADJUST_QTY', payload: {id: product_id, qty: value }})
+        adjustQty: (product_id, value) => dispatch({ type: 'ADJUST_QTY', payload: { id: product_id, qty: value } })
     }
 }
 
@@ -138,37 +140,37 @@ export default connect(mapStateToProps, mapDispatchToProps)(CartItems);
 
 const styles = StyleSheet.create({
     container: {
-        display:'flex',
-        flexDirection:'column',
+        display: 'flex',
+        flexDirection: 'column',
     },
-    itemDescription:{
-        fontFamily:'Helvetica',
-        fontSize:20,
+    itemDescription: {
+        fontFamily: 'Helvetica',
+        fontSize: 20,
     },
-    itemPrice:{
-        fontFamily:'Helvetica',
-        fontSize:20,
-
-
-    },
-    itemQty:{
-        fontFamily:'Helvetica',
+    itemPrice: {
+        fontFamily: 'Helvetica',
+        fontSize: 20,
 
 
     },
+    itemQty: {
+        fontFamily: 'Helvetica',
 
-    finishOrderView:{
-        fontFamily:'Helvetica-Bold',
-        alignSelf:'center'
 
     },
 
-    finishOrderText:{
-        fontSize:25,
-        color:'rgb(64,3,218)'
+    finishOrderView: {
+        fontFamily: 'Helvetica-Bold',
+        alignSelf: 'center'
+
+    },
+
+    finishOrderText: {
+        fontSize: 25,
+        color: 'rgb(64,3,218)'
 
     }
-// <Text style={{ fontFamily: 'Montserrat', fontSize: 20 }}>Montserrat</Text>
+    // <Text style={{ fontFamily: 'Montserrat', fontSize: 20 }}>Montserrat</Text>
 
 
 });
